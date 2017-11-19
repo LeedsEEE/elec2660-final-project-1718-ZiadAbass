127036f295b8b0a6586a327aec8b97d95127534d
Ziad Abass <Ziad@ziads-mac>
2017-11-19 16:22:53 +0000

    Background added

--- Remind Me There/NewReminderViewController.m
+++ Remind Me There/NewReminderViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //Setting backgound image:
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"AbstractBackground"]]];
+    
 }
 
 
--- Remind Me There/ViewReminderViewController.m
+++ Remind Me There/ViewReminderViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //Setting backgound image:
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"AbstractBackground"]]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
