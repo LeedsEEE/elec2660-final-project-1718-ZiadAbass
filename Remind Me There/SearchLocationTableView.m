3042c6f9567cc05c6190658ab799457d5ec50420
Ziad Abass <Ziad@ziads-mac.local>
2017-11-23 00:38:13 +0000

    Number of sections and rows in the results table are set.

--- Remind Me There/SearchLocationTableView.m
+++ Remind Me There/SearchLocationTableView.m
@@ -53,13 +53,13 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    
+    // Number of rows = number of search matches
+    return self.matchingSearchResults.count;
 }
 
 /*
