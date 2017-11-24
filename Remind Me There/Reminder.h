b827d3f81b10e2cc9b836d23027871b7b8440a1b
Ziad Abass <Ziad@ziads-mac>
2017-11-24 23:15:23 +0000

    'Reminder' class created to define within it the properties common to all reminders.

---
+++ Remind Me There/Reminder.h
@@ -0,0 +1,19 @@
+//
+//  Reminder.h
+//  Remind Me There
+//
+//  Created by Ziad Abass on 11/24/17.
+//  Copyright © 2017 University Of Leeds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Reminder : NSObject
+
+@property (strong, nonatomic) NSString *reminderLabel;
+@property (strong, nonatomic) NSString *reminderText;
+@property (strong, nonatomic) NSData *reminderPhotoData;
+@property float *reminderLongtitude;
+@property float *reminderLatitude;
+
+@end
---
+++ Remind Me There/Reminder.m
@@ -0,0 +1,13 @@
+//
+//  Reminder.m
+//  Remind Me There
+//
+//  Created by Ziad Abass on 11/24/17.
+//  Copyright © 2017 University Of Leeds. All rights reserved.
+//
+
+#import "Reminder.h"
+
+@implementation Reminder
+
+@end
