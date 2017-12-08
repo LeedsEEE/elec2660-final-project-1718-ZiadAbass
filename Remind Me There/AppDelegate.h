5a556dfcb7bb9921594de4bef6173ca219492238
Ziad Abass <Ziad@ziads-mac.local>
2017-12-08 09:42:31 +0000

    Launch Image updated. Final push.

--- Remind Me There/AppDelegate.h
+++ Remind Me There/AppDelegate.h
@@ -15,19 +15,3 @@
 
 @end
 
-
-/*
- 
- REFERENCES
- 
-    https://www.youtube.com/watch?v=T7COfFjhXo8
-    Taking and uploading photos methods in NewReminderViewController.
- 
-    For map search help: https://github.com/ThornTechPublic/MapKitTutorialObjC
-    No code was taken directly.
- 
-    https://www.youtube.com/watch?v=73duDCjOTFw
-    Local notification calling in NewReminderViewController (Within didEnterRegion method).
-
-
-*/
--- Remind Me There/main.m
+++ Remind Me There/main.m
@@ -14,3 +14,22 @@ int main(int argc, char * argv[]) {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+
+
+
+/*
+ 
+ REFERENCES
+ 
+ https://www.youtube.com/watch?v=T7COfFjhXo8
+ Taking and uploading photos methods in NewReminderViewController.
+ 
+ For map search help: https://www.thorntech.com/2016/01/how-to-search-for-location-using-apples-mapkit/
+    & https://github.com/ThornTechPublic/MapKitTutorialObjC
+    No code was taken directly from that code.
+ 
+ https://www.youtube.com/watch?v=73duDCjOTFw
+ Local notification calling in NewReminderViewController (Within didEnterRegion method).
+ 
+ 
+ */
